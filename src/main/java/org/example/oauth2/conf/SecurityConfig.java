61b9efec4534a6b7fde776851d21648a8fa5432a
Alexander Kombeiz <alexkombie@gmail.com>
2023-08-24 13:52:32 +0200

    WIP user roles are set as authorities now

--- src/main/java/org/example/oauth2/conf/SecurityConfig.java
+++ src/main/java/org/example/oauth2/conf/SecurityConfig.java
@@ -2,90 +2,57 @@ package org.example.oauth2.conf;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
     
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/public").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/public").permitAll().anyRequest().authenticated());
         
-        http.oauth2Login()
-                .defaultSuccessUrl("/home")
-                .userInfoEndpoint()
-                .oidcUserService(oidcUserService());
+        http.oauth2Login();
         
-        http.logout(logout -> logout
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    String logoutUrl = "http://localhost:8180/auth/realms/oauth2-demo-realm/protocol/openid-connect/logout";
-                    response.sendRedirect(logoutUrl);
-                })
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .deleteCookies("JSESSIONID"));
+        http.logout(logout -> logout.logoutSuccessHandler((request, response, authentication) -> {
+            String logoutUrl = "http://localhost:8180/realms/oauth2-demo-realm/protocol/openid-connect/logout";
+            response.sendRedirect(logoutUrl);
+        }).invalidateHttpSession(true).clearAuthentication(true).deleteCookies("JSESSIONID"));
         return http.build();
     }
     
     @Bean
-    public OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
-        DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
-        return (userRequest) -> {
-            OAuth2User oauth2User = delegate.loadUser(userRequest);
-            OidcIdToken idToken = userRequest.getIdToken();
-            Map<String, Object> claims = idToken.getClaims();
-            Collection<GrantedAuthority> authorities = new ArrayList<>();
-            
-            if (claims.containsKey("realm_access")) {
-                Map<String, Object> realmAccess = (Map<String, Object>) claims.get("realm_access");
-                Collection<String> roles = (Collection<String>) realmAccess.get("roles");
-                roles.forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+    public GrantedAuthoritiesMapper userAuthoritiesMapperForKeycloak() {
+        return authorities -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            var authority = authorities.iterator().next();
+            boolean isOidc = authority instanceof OidcUserAuthority;
+            if (isOidc) {
+                var oidcUserAuthority = (OidcUserAuthority) authority;
+                var userInfo = oidcUserAuthority.getUserInfo();
+                if (userInfo.hasClaim("roles")) {
+                    var roles = (Collection<String>) userInfo.getClaim("roles");
+                    mappedAuthorities.addAll(generateAuthoritiesFromClaim(roles, "ROLES_"));
+                }
             }
-            
-            // Add other authorities if needed
-            authorities.addAll(oauth2User.getAuthorities());
-            
-            // Construct the OidcUserInfo
-            OidcUserInfo userInfo = new OidcUserInfo(oauth2User.getAttributes());
-            
-            // Construct the OidcUser
-            return new DefaultOidcUser(authorities, idToken, userInfo, "preferred_username");
+            return mappedAuthorities;
         };
     }
-
-
     
-    /*
-    @Bean
-    AuthenticationSuccessHandler authenticationSuccessHandler() {
-        return new CustomAuthenticationSuccessHandler();
+    Collection<GrantedAuthority> generateAuthoritiesFromClaim(Collection<String> claim, String authorityType) {
+        return claim.stream().map(role -> new SimpleGrantedAuthority(authorityType + role)).collect(Collectors.toList());
     }
-    
-    @Bean
-    AuthenticationFailureHandler authenticationFailureHandler() {
-        return new CustomAuthenticationFailureHandler();
-    }
-*/
-
 }
--- src/main/java/org/example/oauth2/controller/BasicController.java
+++ src/main/java/org/example/oauth2/controller/BasicController.java
@@ -1,41 +1,47 @@
 package org.example.oauth2.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+// TODO error page
+// TODO login redirect
+// TODO logout redirect
+// TODO comms via https
+
+// TODO check permission by username
+// TODO check permission by organization
+
+// TODO add/remove role to user by admin
+// TODO add/remove org to user by admin
+// TODO let admin to everything
 
 @RestController
 public class BasicController {
     
     @GetMapping("/public")
     public String endpoint1() {
-        return "Hello World!";
+        return "Public Hello World!";
     }
     
     @GetMapping("/home")
-    public String endpoint11() {
+    public String endpoint2() {
         return "My not public home";
     }
     
-    @GetMapping("/claims")
-    public String endpoint2(@AuthenticationPrincipal OidcUser user) {
-        System.out.println(user.getAuthorities());
-        return user.getClaims().toString();
-    }
-    
-    @Secured("ROLE_USER")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @GetMapping("/protected")
-    public String endpoint33(@AuthenticationPrincipal OidcUser user) {
-        return String.format("Hello, %s!", user.getClaimAsString("preferred_username"));
+    public String endpoint3(@AuthenticationPrincipal OAuth2User user) {
+        return String.format("Hello to protected User %s!", user.getName());
     }
     
-    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     @GetMapping("/premium")
-    public String endpoint3(@AuthenticationPrincipal OidcUser user) {
-        return String.format("Hello, %s!", user.getClaimAsString("preferred_username"));
+    public String endpoint4(@AuthenticationPrincipal OAuth2User user) {
+        return String.format("Premium to premium User %s!", user.getName());
     }
     
     @GetMapping("/logout")
