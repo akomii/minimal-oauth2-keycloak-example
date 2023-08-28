b96d130f4b043ff6896ba4704ac7358c0f09b703
Alexander Kombeiz <alexkombie@gmail.com>
2023-08-28 14:33:20 +0200

    moved keycloak-specific configuration into keycloak package, enabled sub-component scanning for keycloak package

--- server/src/main/java/org/example/oauth2/server/conf/SecurityConfig.java
+++ server/src/main/java/org/example/oauth2/server/conf/SecurityConfig.java
@@ -6,16 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -39,31 +30,5 @@ public class SecurityConfig {
                 .clearAuthentication(true)
                 .deleteCookies("JSESSIONID"));
         return http.build();
-    }
-    
-    @Bean
-    public GrantedAuthoritiesMapper userAuthoritiesMapperForKeycloak() {
-        return authorities -> {
-            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
-            authorities.stream()
-                    .filter(OidcUserAuthority.class::isInstance)
-                    .map(OidcUserAuthority.class::cast)
-                    .map(OidcUserAuthority::getUserInfo)
-                    .filter(userInfo -> userInfo.hasClaim("roles"))
-                    .map(userInfo -> (Collection<String>) userInfo.getClaim("roles"))
-                    .forEach(roles -> mappedAuthorities.addAll(generateAuthoritiesFromClaim(roles, "ROLE_")));
-            authorities.stream()
-                    .filter(OidcUserAuthority.class::isInstance)
-                    .map(OidcUserAuthority.class::cast)
-                    .map(OidcUserAuthority::getUserInfo)
-                    .filter(groupInfo -> groupInfo.hasClaim("groups"))
-                    .map(groupInfo -> (Collection<String>) groupInfo.getClaim("groups"))
-                    .forEach(groups -> mappedAuthorities.addAll(generateAuthoritiesFromClaim(groups, "GROUP_")));
-            return mappedAuthorities;
-        };
-    }
-    
-    Collection<GrantedAuthority> generateAuthoritiesFromClaim(Collection<String> claim, String authorityType) {
-        return claim.stream().map(role -> new SimpleGrantedAuthority(authorityType + role)).collect(Collectors.toList());
     }
 }
--- server/src/main/java/org/example/oauth2/server/controller/BasicController.java
+++ server/src/main/java/org/example/oauth2/server/controller/BasicController.java
@@ -10,21 +10,6 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// TODO error page
-// TODO login redirect
-// TODO logout redirect
-// TODO comms via https
-
-// TODO check permission by username
-// TODO check permission by organization
-
-// TODO add/remove role to user by admin
-// TODO add/remove org to user by admin
-// TODO let admin to everything
-
-// TODO get organization infos
-// TODO get user attributes
-
 @RestController
 public class BasicController {
     
